40a0ecd66784a50d278b97d1ee93ca5c13097664
destineevaldivia <destvaldivia@gmail.com>
2023-10-06 18:15:40 -0700

    added new data to db

--- server/db.sql
+++ server/db.sql
@@ -8,16 +8,16 @@ CREATE TABLE blog_post (
   location TEXT NOT NULL,
   content TEXT NOT NULL,
   author VARCHAR(255),
-  photo bytea,
+  remote_url TEXT,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP DEFAULT NOW()
 );
 
-INSERT INTO blog_post (title, location, content, author,)
+INSERT INTO blog_post (title, location, content, author, remote_url)
 VALUES 
-    ('Birthday trip extended', 'Mexico', 'On this trip we started in Mexico City, then we went to Oaxaca Centro, Puerto Escondito and San Jose del Pacifico, we celebrated many special occasions like my birthday, day of the dead, and our 1 year anniversary. It was wonderful!', 'Destinee', ''),
-    ('Primavera Sound', 'Spain', 'will fill out later', 'Destinee'),
-    ('Funemployed', 'Brazil', 'will fill out later', 'Destinee');
+    ('Birthday trip extended', 'Mexico', 'On this trip we started in Mexico City, then we went to Oaxaca Centro, Puerto Escondito and San Jose del Pacifico, we celebrated many special occasions like my birthday, day of the dead, and our 1 year anniversary. It was wonderful!', 'Destinee', 'https://github.com/destineevaldivia/blog-w12/blob/07edc9b8483a869ccbeb0d764b83c8196b2d5bd4/client/w12_blog_images/IMG_1580.jpeg'),
+    ('Primavera Sound', 'Spain', 'We went to visit Conrads friends in Barcelona, we went to a big music festival called Primavera Sounds, we hung out in the plazas a lot with friends and ate a lot of great food. At some point we ventured off to San Sebastian and then went to southern Portugal for cliff jumping. We also discovered costeering.', 'Destinee', 'https://github.com/destineevaldivia/blog-w12/blob/07edc9b8483a869ccbeb0d764b83c8196b2d5bd4/client/w12_blog_images/IMG_9104.jpeg'),
+    ('Funemployed', 'Brazil', 'Poppy, Ajay, Conrad and I had the time of our lives. We started in Sao Paolo, visited many places in between, the last was Rio and we got to experience the infamous Carnaval', 'Destinee', 'client/w12_blog_images/IMG_2243.jpeg');
 
 
 ---
